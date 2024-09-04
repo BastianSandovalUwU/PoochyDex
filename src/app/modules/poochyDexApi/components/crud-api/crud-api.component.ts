6e7f4c824a4410dae06eb4af299fa0d4ba8056f1
Bastián <bastiansandoval26@gmail.com>
2024-09-03 23:57:09 -0400

    new things to api connection

--- src/app/modules/poochyDexApi/components/crud-api/crud-api.component.ts
+++ src/app/modules/poochyDexApi/components/crud-api/crud-api.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CreatePokemon, PoochyDexApiService } from '../../services/poochyDexApi.service';
 import { ALL_POKEMON_JOTHO, ALL_POKEMON_KANTO } from '../../../../../../entities/common/poochyApiData';
+import { Games, pokemonCrystalData, pokemonEmeraldData, pokemonFireRedLeafGreenData, pokemonGoldSiverData, pokemonRedBlueData, pokemonRubySapphireData, pokemonYellowData } from '../../../../../../entities/common/game-data';
 
 @Component({
   selector: 'app-crud-api',
@@ -12,6 +13,11 @@ export class CrudApiComponent implements OnInit {
   kantoPokemon: CreatePokemon[] = ALL_POKEMON_KANTO;
   jothoPokemon: CreatePokemon[] = ALL_POKEMON_JOTHO;
 
+  firstGenerationGames: Games[] = [pokemonRedBlueData, pokemonYellowData];
+  secondGenerationGames: Games[] = [pokemonGoldSiverData, pokemonCrystalData];
+  thirdGenerationGames: Games[] = [pokemonRubySapphireData, pokemonEmeraldData, pokemonFireRedLeafGreenData];
+
+
   constructor(private poochyDexApiService: PoochyDexApiService) { }
 
   ngOnInit() {
@@ -21,16 +27,35 @@ export class CrudApiComponent implements OnInit {
   }
 
   createPokemonData(): void {
+    return;
     this.updateBD(this.kantoPokemon);
   }
   createPokemonDataJotho(): void {
+    return;
     this.updateBD(this.jothoPokemon);
   }
 
-    async updateBD(data: any) {
+  createGenerationGamesData(generation: number): void {
+    switch (generation) {
+      case 1:
+        console.log(this.firstGenerationGames);
+        break;
+      case 2:
+        console.log(this.secondGenerationGames);
+        break;
+      case 3:
+        console.log(this.thirdGenerationGames);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  async updateBD(data: any) {
     for (const poke of data) {
       try {
-        const result = await this.poochyDexApiService.postPokemonAzure(poke).toPromise();
+        const result = await this.poochyDexApiService.postPokemonApi(poke).toPromise();
         console.log(result);
       } catch (error) {
         console.error('Error al guardar Pokémon:', error);
