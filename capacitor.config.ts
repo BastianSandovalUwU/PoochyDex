6f78628723638ce72046ef28a3cbf128f8effc2b
Bastián <bastiansandoval26@gmail.com>
2025-01-26 14:58:49 -0300

    ✨ add android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cl.poochydex.app',
   appName: 'poochydex',
-  webDir: 'dist'
+  webDir: 'dist/poochydex'
 };
 
 export default config;
